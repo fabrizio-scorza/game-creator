1604f86cf6f8a7f0e553ceafca6f7daca587f896
fabrizio-scorza <fabrizio.a.scorza@gmail.com>
2024-05-28 16:21:30 +0200

    ADD: migration items

---
+++ database/migrations/2024_05_28_141352_create_items_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 25);
+            $table->string('slug', 40);
+            $table->string('type', 15);
+            $table->string('category', 40);
+            $table->string('weight', 6);
+            $table->string('cost', 8);
+            $table->string('damage_dice', 5);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('items');
+    }
+};
